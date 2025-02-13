cbb068e24c8a394b26218fdb0750f5b0e0c2b3c7
yoon <sukanyat2022a@gmail.com>
2025-02-13 10:35:40 -0500

    Initial Commit

---
+++ feb13PetSchoolPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  feb13PetSchoolPractice
+//
+//  Created by TS2 on 2/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ feb13PetSchoolPracticeTests/feb13PetSchoolPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  feb13PetSchoolPracticeTests.swift
+//  feb13PetSchoolPracticeTests
+//
+//  Created by TS2 on 2/13/25.
+//
+
+import Testing
+@testable import feb13PetSchoolPractice
+
+struct feb13PetSchoolPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
